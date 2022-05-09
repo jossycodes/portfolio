675fdf951535aabbb4c276893c17e81a0115c77e
Jossylee <105179793+jossycodes@users.noreply.github.com>
2022-05-09 03:03:17 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Nav from '@components/Nav'
 
 export default function Home() {
   return (
@@ -11,7 +12,9 @@ export default function Home() {
         <!-- uikit css -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.14.1/dist/css/uikit.min.css" />
       </Head>
-
+      <nav>
+       <Nav />
+      </nav>
       <main>
         <h1>Hello am Josiah</h1>
       </main>
