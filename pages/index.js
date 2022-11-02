08f9541a83c0fdc4e5acce24ea2a6631b665de4f
jossycodes <josiahadeniyi1@gmail.com>
2022-11-02 23:10:28 +0100

    Edited

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 const [mailBtn,setMailBtn] = useState(false);
   
 useEffect(() => {
-aos.init();
+aos.init({mirror: false}); 
 
 const elms = document.querySelectorAll(".sticky"); 
 const observer = new IntersectionObserver(
